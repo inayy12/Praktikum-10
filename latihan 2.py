15f61923fad0a9a5ee8be16be70427db2e0732a9
inayatus sholekhawati <inayatussholekha12@gmail.com>
2023-01-17 13:15:23 +0700

    simpan

---
+++ latihan 2.py
@@ -0,0 +1,3 @@
+umur = 24
+txt = "Hello, nama saya john, dan umur saya adalah {0} tahun"
+print(txt. format(umur))
---
+++ main.py
@@ -0,0 +1,21 @@
+xt = 'Helllo Wrold'
+pruint(len(txt))
+
+# Mengambil karakter terakhir
+print(txt[-1])
+
+# Mengabil karakter terakhir
+print(txt[2:5])
+
+# Mengambil spasi pada text tersebut (HelloWorld)
+print (txt.replace(" ",""))
+
+# Mengubah taxt menjadi huruf besar
+print(txt.upper())
+
+# Mengubah text menjadi huruf kecil
+print(txt.lower())
+
+# Mengganti karakter H dengan karaker J
+print(txt.replace("H", "J"))
+print()
